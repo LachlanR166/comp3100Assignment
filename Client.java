5a5eba766c667827b203319e83f932dd589981d1
LachlanR166 <68932127+LachlanR166@users.noreply.github.com>
2022-03-23 18:14:05 +1100

    Added GETS + SCHD
    Added both GETS and SCHD loop for jobs until response is NONE.

--- Client.java
+++ Client.java
@@ -1,15 +1,20 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.net.*;
+import java.util.ArrayList;
 
 public class Client {
     Socket s;
     DataOutputStream dout;
     BufferedReader din;
 
+
+    String [] serverList;
+
     public Client(String host, int port){
         try{
             this.s = new Socket(host, port);
-            this.dout = new DataOutputStream(s.getOutputStream());
+            this.dout = new DataOutputStream(this.s.getOutputStream());
             this.din = new BufferedReader(new InputStreamReader(this.s.getInputStream()));
         }
         catch(Exception e){
@@ -19,13 +24,18 @@ public class Client {
 
     public void sendRequest(String request) throws IOException{
         this.dout.write((request + "\n").getBytes());
+        System.out.println("SENT " + request);
     }
 
-    public void receiveResponse() throws IOException{
+    public String receiveResponse() throws IOException{
         String response = this.din.readLine();
+
         if(response.length() > 0){
-            System.out.println(response);
+            System.out.println("RCVD " + response);
         }
+    
+        return response;
+
     }
 
     public void closeConnection() throws IOException{
@@ -42,14 +52,53 @@ public class Client {
         this.receiveResponse();
         this.sendRequest("AUTH " + System.getProperty("user.name"));
         this.receiveResponse();
-        this.sendRequest("REDY");
-        this.receiveResponse();
     }
 
 
+    public String getServerState(String type) throws IOException{
+        System.out.println("\nNext Job: ");
+
+        this.sendRequest("REDY");
+        String response = this.receiveResponse();
+        if(response.split(" ")[0].equals("JOBN")){
+
+            String [] JOBN = response.split(" ");
+            this.sendRequest(String.format("GETS %s %d %d %d", type, Integer.parseInt(JOBN[4]), Integer.parseInt(JOBN[5]), Integer.parseInt(JOBN[6])));
+
+            String [] DATA = this.receiveResponse().split(" ");
+            this.serverList = new String[Integer.parseInt(DATA[1])];
+            this.sendRequest("OK");
+
+            for(int i = 0; i < serverList.length; i++){
+                this.serverList[i] = this.receiveResponse();
+            }
+
+            this.sendRequest("OK");
+            this.receiveResponse();
+
+            this.sendRequest(String.format("SCHD %s %s %s", JOBN[2], this.serverList[0].split(" ")[0], this.serverList[0].split(" ")[1]));
+        }
+
+        else{
+            this.sendRequest("OK");
+        }
+
+        
+        response = this.receiveResponse();
+        return response;
+            
+    }
+
     public static void main(String[] args) throws IOException {
         Client myClient = new Client("localhost", 50000);
+        String response = "";
+
         myClient.handshake();
+
+        while(!response.equals("NONE")){
+        
+            response = myClient.getServerState("Capable");
+        }
         myClient.closeConnection();
     }
 }
