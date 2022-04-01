aca7ccf2901caf1066fdd2e460ed89645bf6b202
LachlanR166 <68932127+LachlanR166@users.noreply.github.com>
2022-04-01 17:51:00 +1100

    Added comments + changed structure
    Added more descriptive comments, changed the overall structure of the code to flow better. Will improve on the modularisation of different algorithms.

--- Client.java
+++ Client.java
@@ -48,19 +48,23 @@ public class Client {
         //Switch case to determine what the correct response should be
         switch(responseArray[0]){
 
+            //If its JCPL then send REDY and start a new job loop
             case "JCPL":
                 this.queJobLoop();
                 break;
 
+            //Else if the first element is JOBN, save the entire string array as latest job and schedule a job
             case "JOBN":
                 this.latestJob = responseArray;
                 this.scheduler();
                 break;
 
+            //Else if the response is DATA, find the largest server type (LRR)
             case "DATA":
                 this.findLargestServerType(response);
                 break;
 
+            //Else if NONE, close the connection to the server
             case "NONE":
                 this.closeConnection();
                 break;
@@ -73,6 +77,22 @@ public class Client {
 
     }
 
+    //Function that handshakes with the ds-sim server
+    public void handshake() throws IOException{
+        this.sendRequest("HELO");
+        this.receiveResponse();
+        this.sendRequest("AUTH " + System.getProperty("user.name"));
+        this.receiveResponse();
+    }
+
+    //Function that starts the next job que
+    public String queJobLoop() throws IOException{
+        this.sendRequest("REDY");            
+
+        return this.receiveResponse();
+            
+    }
+
     //Function to close socket connections and exit the program
     public void closeConnection() throws IOException{
         this.sendRequest("QUIT");
@@ -83,25 +103,9 @@ public class Client {
         System.exit(0);
     }
 
-    //Function that handshakes with the ds-sim server
-    public void handshake() throws IOException{
-        this.sendRequest("HELO");
-        this.receiveResponse();
-        this.sendRequest("AUTH " + System.getProperty("user.name"));
-        this.receiveResponse();
-    }
-
-
-    //Function that starts the next job que
-    public String queJobLoop() throws IOException{
-        this.sendRequest("REDY");            
-
-        return this.receiveResponse();
-            
-    }
-
     //Function that runs once and finds the largest server type and number of servers belonging to that type
     public void findLargestServerType(String response) throws IOException{
+        //Split up the response string into different elements
         String [] DATA = response.split(" ");
 
         this.sendRequest("OK");
@@ -113,17 +117,16 @@ public class Client {
             //Split into an array to access individual elements
             String[] responseArray = response.split(" ");
 
-            //If the core counts are the same and the server types are the same then increase number of server by one
-            if((Integer.parseInt(responseArray[4]) == this.largestCoreCount) && (responseArray[0].equals(this.largestServerType))){
-                this.noOfServers++;
-            }
-
-            //Else If the core count of the current server is larger than the previous and the server type differs; then update it
-            else if(Integer.parseInt(responseArray[4]) > this.largestCoreCount){
+            //If the core count of the current server is larger than the previous and the server type differs; then update it
+            if(Integer.parseInt(responseArray[4]) > this.largestCoreCount){
                 this.largestServerType = responseArray[0];
                 this.largestCoreCount = Integer.parseInt(responseArray[4]);
                 this.noOfServers = 0;
-        
+            }
+
+            //Else if the core counts are the same and the server types are the same then increase number of server by one
+            else if((Integer.parseInt(responseArray[4]) == this.largestCoreCount) && (responseArray[0].equals(this.largestServerType))){
+                this.noOfServers++;
             }
         }
         //Largest server type has been found so is set to true so it wont run again
@@ -135,12 +138,15 @@ public class Client {
         
         //If the largest server type has not yet been found
         if(!hasFoundLargestServerType){
+            //Send GETS All to the server
             this.sendRequest("GETS All");
+            //Server will send DATA in return and trigger findLargestServerType()
             this.receiveResponse();
             this.sendRequest("OK");
             this.receiveResponse();
         }  
 
+        //Schedule the job with the largest server type with rotating server id [i.e "0,1,2,3,0,1,2,3"]
         this.sendRequest(String.format("SCHD %s %s %s", this.latestJob[2], this.largestServerType, this.currentServerIndex));
 
         //If the current server index is greater than or equal to the number of servers then reset to zero
@@ -158,12 +164,16 @@ public class Client {
 
 
     public static void main(String[] args) throws IOException {
+        //Create a new Client on localhost:50000
         Client myClient = new Client("localhost", 50000);
         String response = "";
 
+        //Handshake between client and server
         myClient.handshake();
 
+        //While the response does not equal "NONE"
         while(!response.equals("NONE")){
+            //Que job loop, eventually returning a response
             response = myClient.queJobLoop();
         }
 
